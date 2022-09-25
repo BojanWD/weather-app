45c753c4a41ead1087df37cc526a0417da16928f
Bojan Branovacki <bojan.branovacki@gmail.com>
2022-09-25 08:15:19 +0200

    feat: Add basic structure and fetch function to the app
    Create function to fetch weather data. Create search function to change
    location. Create basic styling for app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,162 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import useUpdateEffect from "./customHooks/useUpdateEffect";
+import cityData from "./data/cityData";
+// components
+import Loading from "./components/Loading";
+import Search from "./components/Search";
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]); // presented weather data
+  const [allWeatherData, setAllWeatherData] = useState({}); // all weather data pulled through an API
+  const [lineGraphData, setLineGraphData] = useState([]);
+  const [currentCity, setCurrentCity] = useState(cityData[1717]); //1717 ns 509 bg
+
+  const [weekIconData, setWeekIconData] = useState([]);
+  const [cOrF, setCOrF] = useState("c"); // Celsius or Fahrenheit
+  const [index, setIndex] = useState(0); // Index denoting day of the week - 0 is today
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [currentCity]);
+
+  useUpdateEffect(() => {
+    if (allWeatherData.current === undefined) {
+      return;
+    }
+    presentedWeatherData(index);
+  }, [cOrF, allWeatherData]);
+
+  const kelvinToCorF = (temp) => {
+    let res;
+    if (cOrF === "f") {
+      res = (temp - 273.15) * 1.8 + 32;
+    }
+    if (cOrF === "c") {
+      res = temp - 273.15;
+    }
+
+    return res;
+  };
+
+  const fetchWeatherData = async () => {
+    const apiKey = "ab1658aef0966b814887e72544046e26";
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.lat}&lon=${currentCity.lng}&appid=${apiKey}`;
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // Initial setting for the weather data
+      setWeatherData({
+        currentTemp: kelvinToCorF(data.current.temp),
+        feelsLike: kelvinToCorF(data.current.feels_like),
+        humidity: data.current.humidity,
+        windSpeed: data.current.wind_speed,
+        time: data.current.dt,
+        description: data.current.weather[0].description,
+        main: data.current.weather[0].main,
+        precipitation: data.hourly[0].pop,
+      });
+      setWeekIconData([]); //we need this because multiple api calls repeat same days in the array
+      for (const x of data.daily) {
+        setWeekIconData((weekIconData) => {
+          return [
+            ...weekIconData,
+            {
+              dt: kelvinToCorF(x.dt),
+              min: kelvinToCorF(x.temp.min),
+              max: kelvinToCorF(x.temp.max),
+              main: x.weather[0].main,
+            },
+          ];
+        });
+      }
+
+      setLineGraphData([
+        kelvinToCorF(data.daily[0].temp.morn * 0.99),
+        kelvinToCorF(data.daily[0].temp.morn),
+        kelvinToCorF(data.daily[0].temp.day),
+        kelvinToCorF(data.daily[0].temp.eve),
+        kelvinToCorF(data.daily[0].temp.night),
+        kelvinToCorF(data.daily[0].temp.night * 0.99),
+      ]);
+
+      setAllWeatherData(data);
+      setIndex(0);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const presentedWeatherData = (index) => {
+    setWeekIconData([]); //we need this because multiple api calls repeat same days in the array
+    for (const x of allWeatherData.daily) {
+      setWeekIconData((weekIconData) => {
+        return [
+          ...weekIconData,
+          {
+            dt: kelvinToCorF(x.dt),
+            min: kelvinToCorF(x.temp.min),
+            max: kelvinToCorF(x.temp.max),
+            main: x.weather[0].main,
+          },
+        ];
+      });
+    }
+    if (index === 0) {
+      // For today we use different object to set state than for other days of the week for which we use forecast (future) data
+      setWeatherData({
+        currentTemp: kelvinToCorF(allWeatherData.current.temp),
+        feelsLike: kelvinToCorF(allWeatherData.current.feels_like),
+        humidity: allWeatherData.current.humidity,
+        windSpeed: allWeatherData.current.wind_speed,
+        time: allWeatherData.current.dt,
+        description: allWeatherData.current.weather[0].description,
+        main: allWeatherData.current.weather[0].main,
+        precipitation: allWeatherData.hourly[0].pop,
+      });
+      setLineGraphData([
+        kelvinToCorF(allWeatherData.daily[0].temp.morn * 0.99),
+        kelvinToCorF(allWeatherData.daily[0].temp.morn),
+        kelvinToCorF(allWeatherData.daily[0].temp.day),
+        kelvinToCorF(allWeatherData.daily[0].temp.eve),
+        kelvinToCorF(allWeatherData.daily[0].temp.night),
+        kelvinToCorF(allWeatherData.daily[0].temp.night * 0.99),
+      ]);
+      return;
+    }
+    setWeatherData({
+      currentTemp: kelvinToCorF(allWeatherData.daily[index].temp.day),
+      feelsLike: kelvinToCorF(allWeatherData.daily[index].feels_like.day),
+      humidity: allWeatherData.daily[index].humidity,
+      windSpeed: allWeatherData.daily[index].wind_speed,
+      time: allWeatherData.daily[index].dt,
+      description: allWeatherData.daily[index].weather[0].description,
+      main: allWeatherData.daily[index].weather[0].main,
+      precipitation: allWeatherData.daily[index].pop,
+    });
+    setLineGraphData([
+      kelvinToCorF(allWeatherData.daily[index].temp.morn * 0.99),
+      kelvinToCorF(allWeatherData.daily[index].temp.morn),
+      kelvinToCorF(allWeatherData.daily[index].temp.day),
+      kelvinToCorF(allWeatherData.daily[index].temp.eve),
+      kelvinToCorF(allWeatherData.daily[index].temp.night),
+      kelvinToCorF(allWeatherData.daily[index].temp.night * 0.99),
+    ]);
+  };
+
+  const changeLocation = (index) => {
+    setCurrentCity(cityData[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Search changeCity={changeLocation} />
+      {loading && <Loading />}
+      {!loading && <></>}
+    </main>
   );
 }
 
---
+++ src/components/Search.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import cityData from "../data/cityData";
+import styles from "../css/search.module.css";
+
+export default function Search({ changeLocation }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
+
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = cityData.filter((value) => {
+      return (
+        value.city.toLowerCase().includes(searchWord.toLowerCase()) ||
+        value.country.toLowerCase().includes(searchWord.toLowerCase())
+      );
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const handleSearch = (element) => {
+    setFilteredData([]);
+    setWordEntered("");
+    let index = cityData.indexOf(element);
+    changeLocation(index);
+  };
+
+  return (
+    <section>
+      <div className={styles.search}>
+        <div className={styles.searchInputs}>
+          <input
+            type="text"
+            placeholder="Search by location"
+            value={wordEntered}
+            onChange={handleFilter}
+          />
+        </div>
+        {filteredData.length !== 0 && (
+          <div className={styles.dataResults}>
+            {filteredData.slice(0, 15).map((value, index) => {
+              return (
+                <div
+                  className={styles.srcItem}
+                  key={index}
+                  onClick={() => handleSearch(filteredData[index])}
+                >
+                  {`${value.city}, ${value.country}`}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
